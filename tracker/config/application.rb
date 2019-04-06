a8207327ec615fa82d3cac97d009da3b6778559d
Bruno Crivelaro <brunosantoscrivelaro@gmail.com>
2019-04-06 02:50:42 -0300

    Configure generators and set uuid as primary key

--- tracker/config/application.rb
+++ tracker/config/application.rb
@@ -15,5 +15,21 @@ module Tracker
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Config rspec generators
+    config.generators do |g|
+      g.test_framework :rspec
+      g.helper_specs false
+      g.controller_specs false
+      g.view_specs false
+      g.routing_specs false
+      g.request_specs false
+    end
+
+    # Set uuid as primary key
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+      g.orm :active_record, force_foreign_key_type: :uuid
+    end
   end
 end
